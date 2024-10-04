698bb85c30b70131148a37508c0d72eff73b375d
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-04 09:48:02 +0200

    Aggiunte funzioni populateArray e createList (da finire)

--- Es_ArrayColori.php
+++ Es_ArrayColori.php
@@ -7,6 +7,25 @@
 </head>
 <body>
     <?php
+        $num = rand(10,21);
+        $lista[] = popolateArray($num);
+        $color = array("red", "green", "blue", "yellow", "orange");
+        createList($lista, $color);
+
+        function popolateArray($n){
+            for ($i=0; $i < $n; $i++) { 
+                $array[$i] = $i;
+            }
+            return $array;
+        }
+
+        function createList($array, $arrayCol){
+            echo "<ol>";
+            foreach ($array as $i) {
+                echo "<li style:'color:" . $arrayCol[rand(0,5)] . "'>$i</li>";
+            }
+            echo "</ol>";
+        }
     ?>
 </body>
 </html>
